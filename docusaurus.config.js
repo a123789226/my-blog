84386852f7eff632383e16579fd34c125bff39c1
Tim Huang <tim.huang@ucfunnel.com>
2022-11-02 21:59:15 +0800

    Test Push

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'BoXiang Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://github.com',
   baseUrl: '/my-blog/',
